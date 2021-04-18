f81e0c35dd81637b79c7a84528b994709650d2f3
MSM <piss>
2021-04-18 20:00:44 +0300

    Added Question class

--- src/Project.java
+++ src/Project.java
@@ -1,5 +1,27 @@
-public class Project {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Project extends Application {
+    public void start(Stage primaryStage) {
+
+        //
+        // Window set-up
+        //
+        BorderPane mainPane = new BorderPane();
+        Scene mainScene = new Scene(mainPane);
+        primaryStage.setTitle("ICS108 Project");
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+
+        // Buttons
+        Button btnNext = new Button("Next");
+        Button btnPrev = new Button("Previous");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        launch(args);
     }
 }
