53538933932296d2b1110c6f7ab2e39902368a2b
MSM <piss>
2021-04-18 20:49:45 +0300

    Updated code

--- src/Project.java
+++ src/Project.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Project extends Application {
@@ -11,7 +13,7 @@ public class Project extends Application {
         // Window set-up
         //
         BorderPane mainPane = new BorderPane();
-        Scene mainScene = new Scene(mainPane);
+        Scene mainScene = new Scene(mainPane, 525, 500); // Change the size later
         primaryStage.setTitle("ICS108 Project");
         primaryStage.setScene(mainScene);
         primaryStage.show();
@@ -19,9 +21,18 @@ public class Project extends Application {
         // Buttons
         Button btnNext = new Button("Next");
         Button btnPrev = new Button("Previous");
+
+        // Grouping - THIS IS NOT WORKING
+        HBox nextPrev = new HBox(5);
+        nextPrev.getChildren().addAll(btnPrev, btnNext);
+        nextPrev.setAlignment(Pos.BOTTOM_CENTER);
+
+        mainPane.getChildren().addAll(nextPrev);
+
     }
 
     public static void main(String[] args) {
+        System.out.println("Launching...");
         launch(args);
     }
 }
--- src/Question.java
+++ src/Question.java
@@ -1,3 +1,10 @@
 
 public class Question {
+
+    // Stores the answers in an array and the correct choice will be determined by the correct choice index
+    String qText = new String();
+    String[] choices = new String[4];
+    int correctChoiceIndex;
+
+    // NOTE: If you think comparing values to get the correct answer is a better method, then remove correctChoice index.
 }
